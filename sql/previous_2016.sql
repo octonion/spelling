41d03341294f16568ea6b99e2977d6b07fa067c1
Christopher D. Long <galizur@gmail.com>
2016-05-25 20:34:30 -0400

    Words previusly appearing

--- sql/previous_2016.sql
+++ sql/previous_2016.sql
@@ -8,12 +8,14 @@ max,
 p.year
 from scripps.results r
 join (
-select word,count(*) as n,
+select
+word,
+count(*) as n,
 min(round_id) as min,
 max(round_id) as max,
 max(year) as year
 from scripps.results where year<=2015 group by word) p
-  on (p.word)=(r.word)
+  on lower(p.word)=lower(r.word)
 where r.year=2016
 order by n desc
 ) to '/tmp/previous_2016.csv' csv header;
