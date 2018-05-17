a28ee732e1996ec277ac06a6a13e62a8db06082d
Christopher D. Long <galizur@gmail.com>
2018-05-17 17:51:06 -0400

    Added 2012 and 2013.

---
+++ scrapers/scripps_2013.rb
@@ -0,0 +1,55 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+
+require 'csv'
+require 'mechanize'
+
+agent = Mechanize.new{ |agent| agent.history.max_size=0 }
+agent.user_agent = 'Mozilla/5.0'
+
+bad = " "
+
+results = CSV.open("csv/scripps_competitions_2013.csv","w")
+
+base = "https://web.archive.org"
+
+path = '//*[@id="copyBody"]/center/table/tr'
+
+year = 2013
+
+url = 'https://web.archive.org/web/20150401074613/http://spellingbee.com:80/public/results/2013/round_results'
+
+begin
+  page = agent.get(url)
+rescue
+  print "  -> error, retrying\n"
+  retry
+end
+
+page.parser.xpath(path).each_with_index do |tr,i|
+  row = [year,i]
+  tr.xpath("td").each_with_index do |td,j|
+    case j
+    when 0
+      text = td.text.strip rescue nil
+      text.gsub!(bad,"") rescue nil
+      text.gsub!("Round ","") rescue nil
+      text.gsub!(":","") rescue nil
+      if (text=="")
+        text = nil
+      end
+      row += [text]
+    when 1
+      td.xpath("a").each_with_index do |a,k|
+        href = a.attributes["href"].value.strip rescue nil
+        text = a.text.strip rescue nil
+        text.gsub!(bad,"") rescue nil
+        text.gsub!("  "," ") rescue nil
+        results << row + [k,text,base+href]
+      end
+    end
+  end
+  results.flush
+end
+
+results.close
---
+++ scrapers/scripps_results_2012.rb
@@ -0,0 +1,75 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+
+require 'csv'
+require 'mechanize'
+
+agent = Mechanize.new{ |agent| agent.history.max_size=0 }
+agent.user_agent = 'Mozilla/5.0'
+
+bad = " "
+
+competitions = CSV.open("csv/scripps_competitions_2012.csv","r")
+results = CSV.open("csv/scripps_results_2012.csv","w")
+
+base = "https://web.archive.org"
+
+path = '//*[@id="copyBody"]/table/tr[position()>1]'
+
+competitions.each do |c|
+
+  if not(["Spelling","HTML"].include?(c[4]))
+    next
+  end
+
+  year = c[0]
+  round = c[2]
+  url = c[5]
+
+  print "#{year} - #{round}\n"
+
+  begin
+    page = agent.get(url)
+  rescue
+    print "  -> error, retrying\n"
+    retry
+  end
+
+  page.parser.xpath(path).each_with_index do |tr,i|
+    row = [year,round]
+    tr.xpath("td").each_with_index do |td,j|
+      case j
+      when 1
+        td.xpath("a").each_with_index do |a,k|
+          href = a.attributes["href"].value.strip rescue nil
+          text = a.text.strip rescue nil
+          text.gsub!(bad,"") rescue nil
+          text.gsub!("  "," ") rescue nil
+          href = base+href
+          row += [text,href]
+        end
+#      when 3
+#        td.xpath("a").each_with_index do |a,k|
+#          href = a.attributes["href"].value.strip rescue nil
+#          text = a.text.strip rescue nil
+#          text.gsub!(bad,"") rescue nil
+#          text.gsub!("  "," ") rescue nil
+#          title = a.attributes["title"].value.strip rescue nil
+#          row += [text,title,href]
+#        end
+      else
+        text = td.text.strip rescue nil
+        text.gsub!(bad,"") rescue nil
+        text.gsub!("  "," ") rescue nil
+        if (text=="")
+          text = nil
+        end
+        row += [text]
+      end
+    end
+    results << row
+  end
+  results.flush
+end
+
+results.close
